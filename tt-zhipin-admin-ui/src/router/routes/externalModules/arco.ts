c6ebc8045df5a006ed6ee60393d6632517dd6a71
whoiszxl <whoiszxl@gmail.com>
2023-08-04 16:39:17 +0800

    feature(admin):初始化 Vue 管理后台前端界面

---
+++ tt-zhipin-admin-ui/src/router/routes/externalModules/arco.ts
@@ -0,0 +1,10 @@
+export default {
+  path: 'https://github.com/whoiszxl/tt-zhipin',
+  name: 'github',
+  meta: {
+    locale: 'menu.github',
+    icon: 'icon-link',
+    requiresAuth: true,
+    order: 100,
+  },
+};
---
+++ tt-zhipin-admin-ui/src/views/login/locale/zh-CN.ts
@@ -0,0 +1,22 @@
+export default {
+  'login.form.title': '登录 tt-zhipin',
+  'login.form.userName.errMsg': '用户名不能为空',
+  'login.form.password.errMsg': '密码不能为空',
+  'login.form.login.errMsg': '登录出错，轻刷新重试',
+  'login.form.login.success': '欢迎使用',
+  'login.form.userName.placeholder': '用户名：admin',
+  'login.form.password.placeholder': '密码：admin',
+  'login.form.rememberPassword': '记住密码',
+  'login.form.forgetPassword': '忘记密码',
+  'login.form.login': '登录',
+  'login.form.register': '注册账号',
+  'login.form.captcha.placeholder': '请输入验证码',
+
+  'login.banner.slogan1': '企业头头与打工仔直接在线开聊',
+  'login.banner.subSlogan1': '让你跟冷冰冰的简历投递说再见',
+  'login.banner.slogan2': '中外各大企业',
+  'login.banner.subSlogan2': '都在用TT直聘招人',
+  'login.banner.slogan3': '去除中间商赚差价的环节',
+  'login.banner.subSlogan3': '简历+行为数据匹配等方式提升你找工作效率',
+
+};
